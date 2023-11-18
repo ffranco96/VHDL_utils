761a71892847f20b9468b567109c0096e6179a8c
Cristian Marrone <marrone62475@estudiantes.untref.edu.ar>
2023-11-18 20:30:17 -0300

    Alu fusionada y con test_bench ok
    Acomode las señales con sus 32bits y la salida Zero. Agregue el program 2 para que ya quede

--- TPE1_ALU/ALU_tstbnch1.vhd
+++ TPE1_ALU/ALU_tstbnch1.vhd
@@ -3,67 +3,67 @@ USE ieee.std_logic_1164.all;
 
 ENTITY ALU_vhd_tst IS
 END ALU_vhd_tst;
-ARCHITECTURE ALU_vhd_tst_arch OF ALU_vhd_tst IS
+ARCHITECTURE behavioural OF ALU_vhd_tst IS
 -- constants                                                 
 -- signals                                                   
-SIGNAL A : STD_LOGIC_VECTOR(7 DOWNTO 0);
-SIGNAL B : STD_LOGIC_VECTOR(7 DOWNTO 0);
-SIGNAL Ctrl : STD_LOGIC_VECTOR(2 DOWNTO 0);
-SIGNAL Res : STD_LOGIC_VECTOR(7 DOWNTO 0);
-SIGNAL Zero : STD_LOGIC;
+        SIGNAL A : STD_LOGIC_VECTOR(31 DOWNTO 0);
+        SIGNAL B : STD_LOGIC_VECTOR(31 DOWNTO 0);
+        SIGNAL Control : STD_LOGIC_VECTOR(2 DOWNTO 0);
+        SIGNAL Result : STD_LOGIC_VECTOR(31 DOWNTO 0);
+        SIGNAL Zero : STD_LOGIC;
 
-COMPONENT ALU
+        COMPONENT ALU
 	port (A : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- Operator A
-	      B : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- Operator B
-        Ctrl : in std_logic_vector(2 downto 0); -- Mode
-        Res: out STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Result
+	B : IN STD_LOGIC_VECTOR(7 DOWNTO 0); -- Operator B
+        Control : in std_logic_vector(2 downto 0); -- Mode
+        Result: out STD_LOGIC_VECTOR(7 DOWNTO 0);  -- Result
         Zero: out STD_LOGIC                     -- Zero
         );
-END COMPONENT;
+        END COMPONENT;
 BEGIN
-	i1 : ALU
+        i1 : ALU
 	PORT MAP (
 -- list connections between master ports and signals
 	A => A,
-    B => B,
-    Ctrl => Ctrl,
-    Res=> Res,
-    Zero=>Zero
-    );
+        B => B,
+        Control => Control,
+        Result=> Result,
+        Zero=>Zero
+        );
 
-always : PROCESS                                              
+        always : PROCESS                                              
 -- optional sensitivity list                                  
 -- (        )                                                 
 -- variable declarations                                      
-BEGIN                                                         
+        BEGIN                                                         
         -- code executes for every event on sensitivity list  
 
--- Prueba con datos
-A <= "10100000";
-B <= "01000001";
+        -- Prueba con datos
+        A <= x"10100000";
+        B <= x"01000001";
 
-Ctrl <= "000";
-wait for 10 ns;
+        Control <= "000";
+        wait for 10 ns;
 
-Ctrl <= "001";
-wait for 10 ns;
+        Control <= "001";
+        wait for 10 ns;
 
-Ctrl <= "010";
-wait for 10 ns;
+        Control <= "010";
+        wait for 10 ns;
 
-Ctrl <= "110";
-wait for 10 ns;
+        Control <= "110";
+        wait for 10 ns;
 
-Ctrl <= "111";
-wait for 10 ns;
+        Control <= "111";
+        wait for 10 ns;
 
-A <= "00000111";
-B <= "00001001";
-wait for 10 ns;
+        A <= x"00000111";
+        B <= x"00001001";
+        wait for 10 ns;
 
-Ctrl <= "100";
-wait for 10 ns;
+        Control <= "100";
+        wait for 10 ns;
 
-WAIT;                                                        
-END PROCESS always;                                          
-END ALU_vhd_tst_arch;
+        WAIT;                                                        
+        END PROCESS always;                                          
+END behavioural;
