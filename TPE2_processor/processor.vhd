295adf0b15c2127a1dbe59dfcc6c7061653a5401
Franco Macén <macen38787@estudiantes.untref.edu.ar>
2023-11-24 02:42:51 -0300

    Uncommented ALU and reordered signals in process. Fixed syncronization of signal MEM_WB_ALU_Res

--- TPE2_processor/processor.vhd
+++ TPE2_processor/processor.vhd
@@ -47,14 +47,14 @@ component control_unit
 		);
 end component;
 
--- component ALU @todo uncomment
--- 	port (A :       IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Operator A @todo extenderlos a 32 bits
--- 	      B :       IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Operator B
---         Control : in std_logic_vector(2 downto 0); -- Control
---         Result:   out STD_LOGIC_VECTOR(31 DOWNTO 0);  -- Result
---         Zero:     out STD_LOGIC                     -- Zero
---         );
--- end component;
+component ALU
+	port (A :       IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Operator A @todo extenderlos a 32 bits
+	      B :       IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Operator B
+        Control : in std_logic_vector(2 downto 0); -- Control
+        Result:   out STD_LOGIC_VECTOR(31 DOWNTO 0);  -- Result
+        Zero:     out STD_LOGIC                     -- Zero
+        );
+end component;
 ---------------------------------------------------------------------------------------------------------------
 --SIGNALS DECLARATION--
 ---------------------------------------------------------------------------------------------------------------
@@ -82,12 +82,14 @@ signal ID_EX_read_data_2 : std_logic_vector(31 downto 0);
 signal EX_Mux_input_B_ALU : std_logic_vector(31 downto 0);
 signal ALU_Control_Res: std_logic_vector(2 downto 0);
 signal Alu_TYPE_R: std_logic_vector(2 downto 0);
+signal EX_ALU_Res : std_logic_vector(31 downto 0);
+signal EX_ALU_Zero : std_logic;
 
 --EX/MEM SEGMENTATION REG--
 signal EX_MEM_control_signals: std_logic_vector (9 downto 0);
 signal EX_MEM_instr : std_logic_vector(31 downto 0);
 signal EX_MEM_ALU_Res : std_logic_vector(31 downto 0); --@todo assign alu res here 
-signal EX_MEM_ALU_Zero : std_Logic;
+signal EX_MEM_ALU_Zero : std_logic;
 -- signal EX_MEM_pc4_extend : std_logic_vector(31 downto 0); --PC + 4 + (extend shift left 2)@todo uncomment
 
 --MEM STAGE--
@@ -159,38 +161,34 @@ ID_EX_extended_imm <= x"0000" & ID_EX_instr(15 downto 0);
 ---------------------------------------------------------------------------------------------------------------
 -- EX STAGE
 ---------------------------------------------------------------------------------------------------------------
-
-
-
--- @todo: instanciar ALU sumador,--@todo temporary just for test
 -- EX_MEM_pc4_extend <= ID_EX_extended_imm sll 2 + ID_EX_pc4; @todo uncomment
 
 EX_Mux_input_B_ALU <= ID_EX_extended_imm when ID_EX_control_signals(8)='1' else
  		               ID_EX_read_data_2 when ID_EX_control_signals(8) = '0' else  --@todo Assign read data 2 from registers here :
 					   x"00000000";
 
--- Señal que recibe Instrucciones (5 down to 0) para Alu_control  @todo uncomment
--- Alu_TYPE_R <= "010" when ID_EX_instr(5 downto 0) = "100000" else                    -- Type_R func add
---                 "110" when ID_EX_instr(5 downto 0) = "100010" else                  -- Type_R func substraction
---                 "000" when ID_EX_instr(5 downto 0) = "100100" else                  -- Type_R func and
---                 "111" when ID_EX_instr(5 downto 0) = "101010" else                  -- Type_R func slt
---                 "001" when ID_EX_instr(5 downto 0) = "100101";                      -- Type_R func or
+-- Señal que recibe Instrucciones (5 down to 0) para Alu_control
+Alu_TYPE_R <= "010" when ID_EX_instr(5 downto 0) = "100000" else                    -- Type_R func add
+                "110" when ID_EX_instr(5 downto 0) = "100010" else                  -- Type_R func substraction
+                "000" when ID_EX_instr(5 downto 0) = "100100" else                  -- Type_R func and
+                "111" when ID_EX_instr(5 downto 0) = "101010" else                  -- Type_R func slt
+                "001" when ID_EX_instr(5 downto 0) = "100101";                      -- Type_R func or
 
 -- -- Alu control, recibe de Control_unit :control_signals					   
--- ALU_Control_Res <= "010" when ID_EX_control_signals(2 downto 0) = "000" else                    -- lw, sw : op alu add
--- 					Alu_TYPE_R	when ID_EX_control_signals(2 downto 0) = "010" else             -- R-type
---                     "110" when ID_EX_control_signals(2 downto 0) = "001" else                   -- Beq :    op alu substraction
--- 					"000" when ID_EX_control_signals(2 downto 0) = "100" else                   -- Andi :   op alu and
--- 					"100" when ID_EX_control_signals(2 downto 0) = "101" else                   -- LUI :    op alu shift left
--- 					"001" when ID_EX_control_signals(2 downto 0) = "110";                       -- Ori :    op alu or
+ALU_Control_Res <= "010" when ID_EX_control_signals(2 downto 0) = "000" else                    -- lw, sw : op alu add
+					Alu_TYPE_R	when ID_EX_control_signals(2 downto 0) = "010" else             -- R-type
+                    "110" when ID_EX_control_signals(2 downto 0) = "001" else                   -- Beq :    op alu substraction
+					"000" when ID_EX_control_signals(2 downto 0) = "100" else                   -- Andi :   op alu and
+					"100" when ID_EX_control_signals(2 downto 0) = "101" else                   -- LUI :    op alu shift left
+					"001" when ID_EX_control_signals(2 downto 0) = "110";                       -- Ori :    op alu or
 
---ALU instantiation @todo uncomment
--- Alu_inst: ALU	
---     port map (		A => ID_EX_read_data_1(31 downto 0),          -- Operator A 
--- 	                B => EX_Mux_input_B_ALU(31 DOWNTO 0),   -- Operator B
---                     Control => ALU_Control_Res(2 downto 0), -- Control
---                     Result => EX_MEM_ALU_Res(31 downto 0),  -- Result
---                     Zero=> EX_MEM_ALU_Zero );                -- Zero
+--ALU instantiation
+Alu_inst: ALU	
+    port map (		A => ID_EX_read_data_1(31 downto 0),        -- Operator A 
+	                B => EX_Mux_input_B_ALU(31 DOWNTO 0),   	-- Operator B
+                    Control => ALU_Control_Res(2 downto 0), 	-- Control
+                    Result => EX_ALU_Res(31 downto 0),  		-- Result
+                    Zero=> EX_ALU_Zero );                		-- Zero
 
 --------------------------------------------------------------------------------------------------------------
 -- EX/MEM SEGMENTATION REG
@@ -201,7 +199,7 @@ EX_Mux_input_B_ALU <= ID_EX_extended_imm when ID_EX_control_signals(8)='1' else
 ---------------------------------------------------------------------------------------------------------------
 -- MEM/WB SEGMENTATION REG
 ---------------------------------------------------------------------------------------------------------------
-MEM_WB_ALU_Res <= EX_MEM_ALU_Res;
+
 ---------------------------------------------------------------------------------------------------------------
 -- WB STAGE
 ---------------------------------------------------------------------------------------------------------------
@@ -229,17 +227,16 @@ moveControlSignalsThroughStages:
 			-- ID STAGE
 			ID_EX_instr <= IF_ID_instr;
 
-			-- EX MEM STAGE
+			-- EX STAGE
 			EX_MEM_instr <= ID_EX_instr;
-
-			MEM_WB_instr <= EX_MEM_instr; --@todo
-			
-			-- EX STAGE: 
-			-- @todo: instanciar ALU sumador, para direccion de branch, sumador para branch
+			EX_MEM_ALU_Zero <= EX_ALU_Zero;
+			EX_MEM_ALU_Res <= EX_ALU_Res;
 			EX_MEM_ALU_Res <= ID_EX_read_data_1 + EX_Mux_input_B_ALU  ;
 			
 			-- MEM STAGE:
+			MEM_WB_instr <= EX_MEM_instr;
 			MEM_WB_Data_Mem_In <= D_DataIn;
+			MEM_WB_ALU_Res <= EX_MEM_ALU_Res;
 			
 		end if;
 
